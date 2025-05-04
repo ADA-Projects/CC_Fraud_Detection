d17420e216d92dc55dd2785137e58c487e5bfaf3
ADA-Projects <120643053+ADA-Projects@users.noreply.github.com>
2025-05-04 23:19:17 +0200

    name change folder

---
+++ scripts/process.py
@@ -0,0 +1,9 @@
+def preprocess(df):
+    df = df.copy()
+    df = df.drop(columns=['Unnamed: 0'], errors='ignore')
+    df['trans_date_trans_time'] = pd.to_datetime(df['trans_date_trans_time'])
+    df['hour'] = df['trans_date_trans_time'].dt.hour
+    df['day_of_week'] = df['trans_date_trans_time'].dt.dayofweek
+    df = pd.get_dummies(df, columns=['category', 'gender', 'state', 'job', 'merchant'], drop_first=True)
+    df = df.drop(columns=['trans_date_trans_time', 'first', 'last', 'cc_num'], errors='ignore')
+    return df
